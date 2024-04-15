9780d7c54c4a50d32d4ac25782c5752582dae70e
Juliano <kent.nolan.juliano@accenture.com>
2024-04-15 10:18:43 +0800

    color changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div
       className="container my-7 mx-auto p-4 shadow"
-      style={{ maxWidth: 600, backgroundImage: "linear-gradient(-45deg, #eeead5, #dfd8b1)", borderRadius: "30px 30px 30px 30px"}}
+      style={{ maxWidth: 600, backgroundImage: "linear-gradient(-40deg, #eeead5, #d8cf9f)", borderRadius: "30px 30px 30px 30px"}}
     >
       <h6 className="text-center mb-2">
         <span>{dateTimeToday}</span>
